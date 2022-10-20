a8a2dccc4fcb40d967e99aa78f96aa44fa8359cc
Roleeygit <juhaszroli2001@gmail.com>
2022-10-20 08:57:05 +0200

    start

---
+++ fill.sql
@@ -0,0 +1,13 @@
+INSERT INTO courses (course, price) VALUES
+("Laravel", 120000),
+("Java", 130000),
+("PHP", 150000),
+("SQL", 160000),
+("HTML", 100000);
+
+INSERT INTO students(name, email, phone, course_id) VALUES
+("Béla", "bela@net.hu", "123456789", 2),
+("Jani", "jani@net.hu", "123456789", 1),
+("Géza", "geza@net.hu", "123456789", 4),
+("Éva", "eva@net.hu", "123456789", 3),
+("Pista", "pisti@net.hu", "123456789", 5);
